1277a816208ee7d20a7e81e57bba5b80c6919f10
Ivan Belov <kaithearchitect@gmail.com>
2021-03-19 19:05:14 +0300

    replace PublishSubject with BehaviourSubject

--- app/src/main/java/com/a/randomsquare/viewmodel/FirstViewModel.kt
+++ app/src/main/java/com/a/randomsquare/viewmodel/FirstViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.a.randomsquare.util.colorsgenerator.IColorsGenerator
 import com.a.randomsquare.util.heavyobjects.HeavyObject
 import dagger.Lazy
-import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 
 
@@ -13,7 +13,7 @@ class FirstViewModel @Inject constructor(
     private var heavyObject: Lazy<HeavyObject>
 ) : ViewModel() {
 
-    var subject =  PublishSubject.create<Int>()
+    var subject =  BehaviorSubject.create<Int>()
 
     fun generateNewColor(code: Int) {
         subject.onNext(generator.getColor(code))
