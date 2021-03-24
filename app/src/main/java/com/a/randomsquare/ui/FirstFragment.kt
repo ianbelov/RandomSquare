381c3f3f2fe70bc364f89a1b6f9fa9ae7b2e8666
Ivan Belov <kaithearchitect@gmail.com>
2021-03-24 16:22:04 +0300

    add SquareCustomView

--- app/src/main/java/com/a/randomsquare/ui/FirstFragment.kt
+++ app/src/main/java/com/a/randomsquare/ui/FirstFragment.kt
@@ -1,6 +1,8 @@
 package com.a.randomsquare.ui
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,12 +15,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.a.randomsquare.R
-import com.a.randomsquare.util.colorsgenerator.Color
 import com.a.randomsquare.viewmodel.FirstViewModel
 import dagger.android.support.AndroidSupportInjection
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 
@@ -31,8 +29,7 @@ class FirstFragment : Fragment() {
     lateinit var instanceButton: Button
     lateinit var callButton: Button
     lateinit var progressBar: ProgressBar
-    lateinit var square: View
-    lateinit var textView: TextView
+    lateinit var customView: SquareCustomView
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -48,17 +45,17 @@ class FirstFragment : Fragment() {
         generateButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
             viewModel.getColorObservable()
-                .subscribe { onNext -> square.setBackgroundColor(onNext) }
+                .subscribe { onNext -> customView.setCustomBackground(onNext) }
             viewModel.getBackgroundColorObservable().subscribe(
                 { next ->
                     Log.d("Fragment", next.code.toString())
                     view.setBackgroundColor(next.code)
-                    textView.text = next.name
+                    customView.setCustomText(next.name)
                     progressBar.visibility = View.INVISIBLE
                 },
                 {
                     progressBar.visibility = View.INVISIBLE
-                    Toast.makeText(context,"Произошла ошибка", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Произошла ошибка", Toast.LENGTH_SHORT).show()
                     Log.d("Fragment", " error")
                 },
                 {
@@ -80,9 +77,8 @@ class FirstFragment : Fragment() {
         generateButton = view.findViewById(R.id.generateFirstButton)
         instanceButton = view.findViewById(R.id.instanceCounterFirstButton)
         callButton = view.findViewById(R.id.callFirstObjectButton)
-        square = view.findViewById(R.id.firstSquare)
+        customView = view.findViewById(R.id.firstSquare)
         progressBar = view.findViewById(R.id.progressBar)
-        textView = view.findViewById(R.id.firstTextView)
         viewModel = ViewModelProvider(this, viewModelFactory).get(FirstViewModel::class.java)
     }
 
