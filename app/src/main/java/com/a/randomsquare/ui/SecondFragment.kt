c0cedc18af4546c850583e9ac7f61821a12561bb
Ivan Belov <kaithearchitect@gmail.com>
2021-03-25 12:57:42 +0300

    add ViewBinding for SecondFragment

--- app/src/main/java/com/a/randomsquare/ui/SecondFragment.kt
+++ app/src/main/java/com/a/randomsquare/ui/SecondFragment.kt
@@ -12,6 +12,9 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.a.randomsquare.R
+import com.a.randomsquare.databinding.FragmentFirstBinding
+import com.a.randomsquare.databinding.FragmentSecondBinding
+import com.a.randomsquare.viewmodel.FirstViewModel
 import com.a.randomsquare.viewmodel.SecondViewModel
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
@@ -20,11 +23,9 @@ class SecondFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-    lateinit var viewModel: SecondViewModel
-    lateinit var generateButton: Button
-    lateinit var instanceButton: Button
-    lateinit var callButton: Button
-    lateinit var square: View
+    private lateinit var viewModel: SecondViewModel
+    private lateinit var binding: FragmentSecondBinding
+    private lateinit var rootView: View
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -36,31 +37,27 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_second, container, false)
-        init(view)
-        generateButton.setOnClickListener {
+        init()
+        binding.generateSecondButton.setOnClickListener {
             viewModel.generateRandomColor()
         }
-        instanceButton.setOnClickListener {
+        binding.instanceCounterSecondButton.setOnClickListener {
             Toast.makeText(context, viewModel.instanceCount().toString(), Toast.LENGTH_SHORT).show()
         }
-        callButton.setOnClickListener {
+        binding.callSecondObjectButton.setOnClickListener {
             viewModel.callObject()
             Toast.makeText(context, "Provider called", Toast.LENGTH_SHORT).show()
         }
         viewModel.colorCodeLiveData.observe(viewLifecycleOwner, Observer {
-            square.setBackgroundColor(viewModel.colorCodeLiveData.value!!)
+            binding.secondSquare.setBackgroundColor(viewModel.colorCodeLiveData.value!!)
         })
 
         return view
     }
 
-
-    private fun init(view: View) {
-        generateButton = view.findViewById(R.id.generateSecondButton)
-        instanceButton = view.findViewById(R.id.instanceCounterSecondButton)
-        callButton = view.findViewById(R.id.callSecondObjectButton)
-        square = view.findViewById(R.id.secondSquare)
+    private fun init() {
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        rootView = binding.root
         viewModel = ViewModelProvider(this, viewModelFactory).get(SecondViewModel::class.java)
     }
 }
